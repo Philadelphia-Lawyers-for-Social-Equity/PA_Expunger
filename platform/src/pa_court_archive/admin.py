b75063ed175ee8c82237d2065f188df52380b9fb
Travis Southard <tsouthard88@gmail.com>
2021-01-31 16:03:20 -0500

    Normalized data in Pa_court_archive

--- platform/src/pa_court_archive/admin.py
+++ platform/src/pa_court_archive/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
-from .models import PaRecord
+from .models import Arrestee, Case, Docket, Offense
 
-admin.site.register(PaRecord)
+admin.site.register(Arrestee)
+admin.site.register(Case)
+admin.site.register(Docket)
+admin.site.register(Offense)
--- platform/src/pa_court_archive/models.py
+++ platform/src/pa_court_archive/models.py
@@ -1,43 +1,65 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.utils.translation import gettext_lazy as gettext
 
 """
 Provide an interface for the PA Court Data that PLSE purchased.
 """
 
+class GenderCode(models.TextChoices):
+    MALE = "M", gettext("Male")
+    FEMALE = "F", gettext("Female")
+    UNREPORTED = "U", gettext("Unknown/Unreported")
 
-class PaRecord(models.Model):
-    external_mysql_id = models.TextField(unique=True)
-    county_name = models.TextField(null=True)
-    docket_number = models.TextField(null=True)
-    filed_date = models.TextField(null=True)
-    last_name = models.TextField(null=True)
+class RaceCode(models.TextChoices):
+    WHITE = "WHITE", gettext("White")
+    BLACK = "BLACK", gettext("Black")
+    UNREPORTED = "UNREPORTED", gettext("Unknown/Unreported")
+    ASIAN = "ASIAN", gettext("Asian")
+    ASIAN_PACIFIC = "ASIANgettextPACIFIC", gettext("Asian/Pacific Islander")
+    NATIVE = "NATIVE", gettext("Native American/Alaskan Native")
+    BIRACIAL = "BIRACIAL", gettext("Bi-Racial")
+    HAWAIIAN_PACIFIC = "HAWAIIAN_PACIFIC", gettext("Native Hawaiian/Pacific Islander")
+
+class Arrestee(models.Model):
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=["first_name", "last_name", "middle_name", "birth_date"], name="unique_arrestee")]
+    
     first_name = models.TextField(null=True)
+    last_name = models.TextField()
     middle_name = models.TextField(null=True)
+    gender_code = models.CharField(max_length=1, choices=GenderCode.choices, null=True)
+    race_code = models.CharField(max_length=16, choices=RaceCode.choices, null=True)
+    birth_date = models.DateField(null=True)
+
+class Case(models.Model):
+    otn = models.TextField(unique=True, null=False)
+    filed_date = models.DateField(null=True)
     city = models.TextField(null=True)
-    state = models.TextField(null=True)
-    zipcode = models.TextField(null=True)
-    offense_tracking_number = models.TextField(null=True)
-    gender_code = models.TextField(null=True)
-    race_code = models.TextField(null=True)
-    birthdate = models.TextField(null=True)
-    originating_offense_sequence = models.TextField(null=True)
-    statute_type = models.TextField(null=True)
+    county = models.TextField(null=True)
+    state = models.CharField(max_length=2, null=True)
+    zip = models.CharField(max_length= 10, null=True)
+    disposition = models.TextField(null=True)
+    disposition_date = models.DateField(null=True)
+    disposing_judge = models.TextField(null=True)
+    arrestees = models.ManyToManyField(Arrestee, null=True)
+
+class Docket(models.Model):
+    docket_number = models.TextField(unique=True, null=False)
+    case = models.ForeignKey(Case, on_delete=models.CASCADE, null=True)
+
+class Offense(models.Model):
+    disposition = models.TextField(null=True)
+    date = models.DateField(null=True)
+    disposition_date = models.DateField(null=True)
+    description = models.TextField(null=True)
+    originating_sequence = models.IntegerField(null=True)
     statute_title = models.TextField(null=True)
     statute_section = models.TextField(null=True)
     statute_subsection = models.TextField(null=True)
     inchoate_statute_title = models.TextField(null=True)
     inchoate_statute_section = models.TextField(null=True)
     inchoate_statute_subsection = models.TextField(null=True)
-    offense_disposition = models.TextField(null=True)
-    offense_date = models.TextField(null=True)
-    offense_disposition_date = models.TextField(null=True)
-    offense_description = models.TextField(null=True)
-    case_disposition = models.TextField(null=True)
-    case_disposition_date = models.TextField(null=True)
-    offense_grade = models.TextField(null=True)
-    disposing_judge = models.TextField(null=True)
-
-    def __str__(self):
-        return "%s: %s %s" % (self.docket_number, self.first_name, self.last_name)
+    grade = models.CharField(max_length=2, null=True)
+    docket = models.ForeignKey(Docket, on_delete=models.CASCADE, null=True)
