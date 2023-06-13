0398d6b98edc617c97efcc823d5b9fc4b40962cf
joshuagerstein <gerstein.joshua@gmail.com>
2023-06-13 17:20:54 -0400

    Add parsing for court summaries, change tests to use pytest-regressions

--- platform/docket_parser/src/docket_parser/parsing.py
+++ platform/docket_parser/src/docket_parser/parsing.py
@@ -4,6 +4,7 @@ import re
 import traceback
 from collections.abc import Iterable
 from datetime import date
+from enum import StrEnum
 from pathlib import Path
 from typing import Any, BinaryIO, TextIO
 
@@ -22,6 +23,128 @@ REPLACEMENTS = {"NOT_INSERTED_CHARACTER_REGEX": DocketReader.generate_content_re
                 "INSERTED_TAB": DocketReader.tab,
                 "INSERTED_COMES_BEFORE": DocketReader.comes_before,
                 "INSERTED_BOX_WRAP": DocketReader.box_wrap}
+
+
+class DocumentType(StrEnum):
+    COURT_SUMMARY = "court summary"
+    DOCKET = "docket"
+
+    def grammar_path(self) -> Path:
+        if self.value == "court summary":
+            return Path(__file__).parent.joinpath("court_summary_grammar.ppeg")
+        elif self.value == "docket":
+            return Path(__file__).parent.joinpath("docket_grammar.ppeg")
+
+    def visitor(self) -> NodeVisitor:
+        if self.value == "court summary":
+            return CourtSummaryVisitor()
+        elif self.value == "docket":
+            return DocketVisitor()
+
+
+# noinspection PyMethodMayBeStatic, PyUnusedLocal
+class CourtSummaryVisitor(NodeVisitor):
+    string_leaves = ["defendant_name_reversed", "docket_number", "otn", "dcn", "county",
+                     "proc_status", "judge", "grade", "statute", "sequence_number", "disposition", "charge_description"]
+    date_leaves = ["dob", "disposition_date", "arrest_date"]
+
+    def __init__(self) -> None:
+        super().__init__()
+        for leaf_name in self.string_leaves:
+            self.add_leaf_visitor(leaf_name)
+        for date_name in self.date_leaves:
+            self.add_date_visitor(date_name)
+
+    @classmethod
+    def add_leaf_visitor(cls, leaf_name: str) -> None:
+        """Add a visit method for a given leaf name, which returns a dictionary containing only the leaf name as key
+        and the stripped text of node as value.
+        """
+
+        def visit_leaf(self, node, visited_children) -> dict[str, str]:
+            text: str = node.text.strip()
+            text = text.replace(DocketReader.box_wrap, '')
+            logger.debug({leaf_name: text})
+            return {leaf_name: text}
+
+        method_name = "visit_" + leaf_name
+        setattr(cls, method_name, visit_leaf)
+
+    @classmethod
+    def add_date_visitor(cls, date_name: str) -> None:
+        """Add a visit method for a given date name, which returns a dictionary containing only the date name as key
+        and a date object as value.
+        """
+
+        def visit_date(self, node, visited_children) -> dict[str, datetime.date]:
+            date_string = node.text.strip()
+            logger.debug({date_name: date_string})
+            month, day, year = date_string.split("/")
+            return {date_name: date(int(year), int(month), int(day))}
+
+        method_name = "visit_" + date_name
+        setattr(cls, method_name, visit_date)
+
+    def generic_visit(self, node, visited_children) -> Node | list:
+        """Default behavior is to go further down the tree."""
+        return visited_children or node
+
+    def visit_whole_summary(self, node, visited_children) -> dict:
+        summary_info = {"dockets": []}
+        for visited_child in flatten(visited_children):
+            if "docket_number" in visited_child:
+                summary_info["dockets"].append(visited_child)
+            elif isinstance(visited_child, dict):
+                summary_info.update(visited_child)
+        return summary_info
+
+    def visit_aliases(self, node, visited_children) -> dict:
+        aliases = node.text.strip().split(DocketReader.box_wrap)
+        # "WARRANT OUTSTANDING" can show up directly below aliases. Hopefully that's not anyone's actual alias.
+        if "WARRANT" in aliases[-1]:
+            aliases.pop()
+        return {"aliases": aliases}
+
+    def visit_category_section(self, node, visited_children) -> list[dict]:
+        category_name = node.text.split(DocketReader.properties_open)[0].strip()
+        for visited_child in flatten(visited_children):
+            if "docket_number" in visited_child:
+                visited_child["category"] = category_name
+        return visited_children
+
+    def visit_county_section(self, node, visited_children) -> list[dict]:
+        county_name = None
+        for visited_child in flatten(visited_children):
+            if "county" in visited_child:
+                county_name = visited_child.pop("county")
+            if "docket_number" in visited_child:
+                if not county_name:
+                    raise RuntimeError("Failed to find county before docket number")
+                visited_child["county"] = county_name
+        return visited_children
+
+    def visit_docket_section(self, node, visited_children):
+        docket_info = {}
+        for visited_child in flatten(visited_children):
+            if isinstance(visited_child, dict):
+                logger.debug(visited_child)
+                docket_info.update(visited_child)
+        return docket_info
+
+    def visit_charges_section(self, node, visited_children):
+        charges = []
+        for visited_child in flatten(visited_children):
+            if isinstance(visited_child, dict):
+                logger.debug(visited_child)
+                charges.append(visited_child)
+        return {"charges": charges}
+
+    def visit_charge_segment(self, node, visited_children):
+        charge = {}
+        for visited_child in flatten(visited_children):
+            if isinstance(visited_child, dict):
+                charge.update(visited_child)
+        return charge
 
 
 # noinspection PyMethodMayBeStatic, PyUnusedLocal
@@ -68,6 +191,7 @@ class DocketVisitor(NodeVisitor):
 
         def visit_date(self, node, visited_children) -> dict[str, datetime.date]:
             date_string = node.text.strip()
+            logger.debug({date_name: date_string})
             month, day, year = date_string.split("/")
             return {date_name: date(int(year), int(month), int(day))}
 
@@ -155,8 +279,20 @@ class DocketVisitor(NodeVisitor):
 
 # Helpers
 
-def remove_page_breaks(extracted_text: str) -> str:
-    """Remove all page breaks from extracted text.
+def get_document_type(text) -> DocumentType:
+    """ Given the extracted text of a document, return the appropriate instance of DocumentType.
+    If we can't determine document type, raiser error.
+    """
+    line_2 = text.split('\n')[1]
+    if "docket" in line_2.lower():
+        return DocumentType.DOCKET
+    if "court summary" in line_2.lower():
+        return DocumentType.COURT_SUMMARY
+    raise ParseError("Could not determine document type")
+
+
+def remove_docket_page_breaks(extracted_text: str) -> str:
+    """Remove all page breaks text extracted from a docket.
     This allows us to simplify grammar by not needing to check for page breaks everywhere"""
     # This function may be useful in the future but is not currently used.
     input_lines = extracted_text.split(DocketReader.terminator)
@@ -168,7 +304,7 @@ def remove_page_breaks(extracted_text: str) -> str:
     not_props_close = '[^' + props_close + ']*'
     properties_regex = props_open + not_props_close + props_close
     versus_line_regex = r"v\. *" + properties_regex
-    date_regex = r"\d{2}/\d{2}/\d{4}"
+    date_regex = r"\d{1,2}/\d{1,2}/\d{4}"
     printed_date_line_regex = re.compile(r"Printed:\s*" + date_regex + not_props_open + properties_regex)
 
     for index, line in enumerate(input_lines[1:], start=1):
@@ -183,6 +319,56 @@ def remove_page_breaks(extracted_text: str) -> str:
             output_lines.append(line)
 
     return DocketReader.terminator.join(output_lines) + DocketReader.terminator
+
+
+def remove_court_summary_page_breaks(extracted_text: str) -> str:
+    """Remove all page breaks text extracted from a court summary.
+    This allows us to simplify grammar by not needing to check for page breaks everywhere"""
+    input_lines = extracted_text.split(DocketReader.terminator)
+    output_lines = [input_lines[0]]
+    in_page_break = False
+    box_wrap = re.escape(DocketReader.box_wrap)
+    props_open = re.escape(DocketReader.properties_open)
+    not_props_open = '[^' + props_open + ']*'
+    props_close = re.escape(DocketReader.properties_close)
+    not_props_close = '[^' + props_close + ']*'
+    properties_regex = props_open + not_props_close + props_close
+    bold_properties_regex = props_open + not_props_close + 'bold' + props_close
+    continuation_regex = not_props_open + r'\(Continued\)' + bold_properties_regex
+    # Fix case where (Continued) is in a text box with line below it:
+    continuation_textbox_regex = not_props_open + r'\(Continued\)' + box_wrap + not_props_open + bold_properties_regex
+    date_regex = r"\d{1,2}/\d{1,2}/\d{4}"
+    printed_date_line_regex = re.compile(r"Printed:\s*" + date_regex + not_props_open + properties_regex)
+
+    for index, line in enumerate(input_lines[1:], start=1):
+        if in_page_break:
+            if re.match(continuation_regex, input_lines[index - 1]) and \
+                    not re.match(continuation_regex, line):
+                logger.debug(f"end page break matched: {input_lines[index - 1]}")
+                in_page_break = False
+
+                if re.match(continuation_textbox_regex, line):
+                    # Include anything after '(Continued)' textbox wrap
+                    post_page_break = line.split(DocketReader.box_wrap, 1)[1]
+                    output_lines.append(post_page_break)
+                else:
+                    output_lines.append(line)
+        elif re.match(printed_date_line_regex, line):
+            logger.debug(f"begin page break matched: {line}")
+            in_page_break = True
+        else:
+            output_lines.append(line)
+
+    return DocketReader.terminator.join(output_lines) + DocketReader.terminator
+
+
+def remove_page_breaks(extracted_text: str) -> str:
+    """Return extracted text with page breaks removed."""
+    document_type = get_document_type(extracted_text)
+    if document_type == DocumentType.DOCKET:
+        return remove_docket_page_breaks(extracted_text)
+    elif document_type == DocumentType.COURT_SUMMARY:
+        return remove_court_summary_page_breaks(extracted_text)
 
 
 def flatten(visited_children):
@@ -245,17 +431,21 @@ def parse_pdf(file: str | BinaryIO | Path) -> dict[str, str | list[str | dict]]:
 
 
 def parse_extracted_text(text: str) -> dict[str, str | list[dict] | float | datetime.date]:
-    ppeg_file_path = Path(__file__).parent.joinpath("docket_grammar.ppeg")
+    # First determine what kind of document the extracted text is from:
+    document_type = get_document_type(text)
 
-    docket_grammar = get_grammar_from_file(ppeg_file_path)
+    ppeg_path = document_type.grammar_path()
+    grammar = get_grammar_from_file(ppeg_path)
+    if document_type == DocumentType.COURT_SUMMARY:
+        text = remove_page_breaks(text)
 
     try:
-        tree = docket_grammar.parse(text)
+        tree = grammar.parse(text)
     except ParseError as err:
         logger.error("Unable to parse extracted text")
         raise err
 
-    visitor = DocketVisitor()
+    visitor = document_type.visitor()
     parsed = {}
     try:
         parsed = visitor.visit(tree)
--- platform/docket_parser/src/docket_parser/tests/test_parsing.py
+++ platform/docket_parser/src/docket_parser/tests/test_parsing.py
@@ -1,10 +1,10 @@
-import json
+from pathlib import Path
 
 import pytest
 from deepdiff import DeepDiff
 
-from docket_parser import test_data_path
-from docket_parser.parsing import flatten, parse_extracted_text, parse_pdf, remove_page_breaks
+from docket_parser import test_data_path, document_types
+from docket_parser.parsing import flatten, parse_extracted_text, remove_page_breaks
 
 DATA_PATH = test_data_path
 
@@ -18,60 +18,65 @@ def check_expect(check, expected, msg=''):
         pytest.fail(message)
 
 
+def get_extracted_document_paths() -> tuple[list[Path], list[str]]:
+    extracted_document_paths = []
+    for document_type in document_types:
+        paths = (DATA_PATH / document_type / 'extracted').glob("*.txt")
+        extracted_document_paths.extend(paths)
+    ids = [path.stem for path in extracted_document_paths]
+    return extracted_document_paths, ids
+
+
 class TestParsing:
-    def test_multiline_charge(self):
-        """Check one specific example of a multi-line charge description."""
-        test_extracted_text_path = DATA_PATH / "dockets/merge-cp-02.pdf"
-        expected_result_path = DATA_PATH / "json/merge-cp-02.json"
-        with open(expected_result_path, 'rb') as expected_result_file:
-            expected_dict = json.load(expected_result_file)
-        if 'section_disposition' not in expected_dict:
-            raise ValueError(f"Did not find 'section_disposition' in the test file {expected_result_path}")
-        result_dict = parse_pdf(test_extracted_text_path)
-        for index, case_event in enumerate(expected_dict['section_disposition']):
-            result_charges = result_dict['section_disposition'][index]['charges']
-            expected_charges = case_event['charges']
-            check_expect(result_charges, expected_charges,
-                         "Charge with multi-line description did not get parsed correctly")
+    # Delete this test if/when we decide to use pytest-regressions
+    # @pytest.mark.parametrize('document_type', document_types)
+    # def test_all_documents(self, document_type: str):
+    #     """Regression test, check that the parsed information from each saved extracted text matches
+    #     known correct json"""
+    #     # Consider using pytest-regtest or pytest-canonical-data plugins in future
+    #     extracted_dir = DATA_PATH / document_type / "extracted"
+    #     json_dir = DATA_PATH / document_type / "json"
+    #     test_extracted_paths = tuple(extracted_dir.glob("*.txt"))
+    #     expected_result_paths = []
+    #     for test_extracted_path in test_extracted_paths:
+    #         expected_result_filename = test_extracted_path.name.replace('.txt', '.json')
+    #         expected_result_path = json_dir / expected_result_filename
+    #         expected_result_paths.append(expected_result_path)
+    #
+    #     test_expect_pairs = tuple(zip(test_extracted_paths, expected_result_paths))
+    #     assert len(test_expect_pairs) > 0, "Didn't find any test extracted txt/json result pairs"
+    #     for test_extracted_path, expected_result_path in test_expect_pairs:
+    #         with open(test_extracted_path, 'r', encoding='utf-8') as file:
+    #             extracted_text = file.read()
+    #         result = parse_extracted_text(extracted_text)
+    #         # Convert for comparison
+    #         result = json.loads(json.dumps(result, default=repr))
+    #         with open(expected_result_path, 'rb') as expected_result_file:
+    #             expected = json.load(expected_result_file)
+    #
+    #         failure_msg = f"Parsed result from {test_extracted_path.name} not equal to expected result" \
+    #                       f" from {expected_result_path.name}"
+    #         check_expect(result, expected, failure_msg)
 
-    @pytest.mark.slow
-    def test_all_dockets(self):
-        """Regression test, check that the parsed information from each saved extracted text matches
-        known correct json"""
-        # Consider using pytest-regtest or pytest-canonical-data plugins in future
-        test_extracted_paths = tuple(DATA_PATH.glob("extracted/*.txt"))
-        expected_results_dir = DATA_PATH / "json"
-        expected_result_paths = []
-        for test_extracted_path in test_extracted_paths:
-            expected_result_filename = test_extracted_path.name.replace('.txt', '.json')
-            expected_result_path = expected_results_dir / expected_result_filename
-            expected_result_paths.append(expected_result_path)
+    @pytest.mark.parametrize('extracted_document_path', get_extracted_document_paths()[0],
+                             ids=get_extracted_document_paths()[1])
+    def test_documents(self, data_regression, extracted_document_path):
+        with open(extracted_document_path, 'r', encoding='utf-8') as file:
+            extracted_text = file.read()
+        result = parse_extracted_text(extracted_text)
+        data_regression.check(result)
 
-        test_expect_pairs = tuple(zip(test_extracted_paths, expected_result_paths))
-        assert len(test_expect_pairs) > 0, "Didn't find any test extracted txt/json result pairs"
-        for test_extracted_path, expected_result_path in test_expect_pairs:
-            with open(test_extracted_path, 'r', encoding='utf-8') as file:
-                extracted_text = file.read()
-            result = parse_extracted_text(extracted_text)
-            # Convert for comparison
-            result = json.loads(json.dumps(result, default=repr))
-            with open(expected_result_path, 'rb') as expected_result_file:
-                expected = json.load(expected_result_file)
-
-            failure_msg = f"Parsed result from {test_extracted_path.name} not equal to expected result" \
-                          f" from {expected_result_path.name}"
-            check_expect(result, expected, failure_msg)
-
-
-class TestHelpers:
-
-    def test_remove_page_breaks(self):
-        """Regression test, check that output from remove_page_breaks is equal to known good output"""
-        expected_result_paths = tuple((DATA_PATH / 'page_break').glob('*.txt'))
+    @pytest.mark.parametrize('document_type', document_types)
+    def test_remove_page_breaks(self, document_type):
+        """Regression test, check that output from the functions which remove page breaks
+        are equal to known good output"""
+        page_break_dir = DATA_PATH / document_type / "page_break"
+        extracted_dir = DATA_PATH / document_type / "extracted"
+        expected_result_paths = tuple(page_break_dir.glob('*.txt'))
         test_paths = []
         for expected_result_path in expected_result_paths:
             test_filename = expected_result_path.name.replace('-no-page-break', '')
-            test_path = DATA_PATH / 'extracted' / test_filename
+            test_path = extracted_dir / test_filename
             if test_path.is_file():
                 test_paths.append(test_path)
             else:
@@ -84,8 +89,10 @@ class TestHelpers:
             with open(expected_result_path, 'r', encoding='utf-8') as file:
                 expected = file.read()
             result = remove_page_breaks(extracted_text)
-            assert expected == result, "Difference found between remove_page_breaks output and expected result."
+            assert expected == result, "Difference found between remove page breaks output and expected result."
 
+
+class TestHelpers:
     def test_flatten_already_flat(self):
         """Ensure we leave flat lists unchanged."""
         assert list(flatten([])) == []
