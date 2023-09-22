1d0ec8b21f57b2993e9407d9e52a342e8f0f6446
Caitlin Pauze Sherman <caitlinpSherman@gmail.com>
2023-09-22 14:15:15 -0400

    Sign Up in nav bar now switches to Profile link if user is logged in

--- frontend/src/src/components/nav.js
+++ frontend/src/src/components/nav.js
@@ -1,39 +1,46 @@
-import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
-import { useAuth } from "../context/auth";
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { useAuth } from '../context/auth';
 
 const Navigation = () => {
   const { logout, authTokens } = useAuth();
 
-  return <Navbar 
-    collapseOnSelect 
-    expand="lg" 
-    bg="light" 
-    variant="light" 
-    inverse="true" 
-    fluid="true"
-  >
-    <Navbar.Brand href="/">
-      <img
-        src="http://plsephilly.org/wp-content/uploads/2014/11/PLSE_logotype_320.png"
-        width="90"
-        height="30"
-        className="d-inline-block align-top"
-        alt="PLSE logo"
-      />
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="ml-auto">
-        <Nav.Link href="/signup">
-          Sign up
-        </Nav.Link>
-
-        {authTokens ? <Nav.Link onClick={logout}>Log out</Nav.Link> : <Nav.Link href="/login">Log in</Nav.Link>}
-
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+  return (
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="light"
+      variant="light"
+      inverse="true"
+      fluid="true"
+    >
+      <Navbar.Brand href="/">
+        <img
+          src="http://plsephilly.org/wp-content/uploads/2014/11/PLSE_logotype_320.png"
+          width="90"
+          height="30"
+          className="d-inline-block align-top"
+          alt="PLSE logo"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          {authTokens ? (
+            <>
+              <Nav.Link href="/profile">Profile</Nav.Link>
+              <Nav.Link onClick={logout}>Log out</Nav.Link>
+            </>
+          ) : (
+            <>
+              <Nav.Link href="/signup">Sign up</Nav.Link>
+              <Nav.Link href="/login">Log in</Nav.Link>
+            </>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
 export default Navigation;
