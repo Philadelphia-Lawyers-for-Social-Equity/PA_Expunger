c9377c82dd658ed6ec7aa0f24e4702bcdcedb51a
Sabrina Ko <shkteddy@gmail.com>
2023-09-27 14:06:05 -0400

    cleaned up auth context

--- frontend/src/src/App.js
+++ frontend/src/src/App.js
@@ -1,39 +1,23 @@
-import React, { useState } from "react";
-//import axios from 'axios';
+import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom"; // removed Switch
 import PrivateRoute from "./PrivateRoute";
-import { AuthContext } from "./context/auth";
-//import AdminPage from "../src/components/AdminPage";
+import { AuthProvider } from "./context/auth";
+import { UserProvider } from "./context/user";
 import LoginForm from "../src/components/LoginForm";
 import ChooseAction from "../src/components/ChooseAction";
 import SearchPage from "../src/components/SearchPage";
 import LandingPage from "../src/components/LandingPage";
 import FileUpload from "../src/components/FileUpload";
-// import InputForm from "../src/components/InputForm";
 import GeneratePage from "./components/GeneratePage";
 import ProfilePage from "../src/components/ProfilePage";
 import BodyBackgroundColor from "react-body-backgroundcolor";
-
 import SignUp from "./components/SignUp/signUp";
 import Nav from "./components/nav";
-import { UserProvider } from "./context/user";
 
 function App(props) {
-  const [authTokens, setAuthTokens] = useState(JSON.parse(localStorage.getItem("tokens") || null));
-
-  const setTokens = data => {
-    localStorage.setItem("tokens", JSON.stringify(data));
-    setAuthTokens(data);
-  };
-
-  const logout = () => {
-    localStorage.removeItem("tokens");
-    setAuthTokens(null);
-  };
-
   return (
-    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens, logout }}>
+    <AuthProvider>
       <UserProvider>
         <Router>
           <Nav />
@@ -70,7 +54,7 @@ function App(props) {
           </BodyBackgroundColor>
         </Router>
       </UserProvider>
-    </AuthContext.Provider>
+    </AuthProvider>
   );
 }
 
--- frontend/src/src/context/auth.js
+++ frontend/src/src/context/auth.js
@@ -1,11 +1,25 @@
-import { createContext, useContext } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
-export const AuthContext = createContext({
-  authTokens: null,
-  setAuthTokens: () => {},
-  logout: () => {}
-});
+export const AuthContext = createContext();
 
 export function useAuth() {
   return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [authTokens, setAuthTokens] = useState(null);
+
+  function logout () {
+    setAuthTokens(null);
+  };
+
+  const value = {
+    authTokens, setAuthTokens, logout
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  )
 }
