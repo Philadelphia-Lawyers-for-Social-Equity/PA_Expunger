480a20a31f3187677cf84da44fc8dec42ebf5b11
Gary Pang <gary.pang@gmail.com>
2024-03-08 00:41:08 -0500

    Add text to PageNotFound

--- frontend/src/src/components/PageNotFound/index.js
+++ frontend/src/src/components/PageNotFound/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 function PageNotFound() {
     return (
         <div>
-            <div>The page you were looking for does not exist</div>
+            <h1>404: Page Not Found</h1>
+            <p>The page you're looking for doesn't exist.</p>
+            <p>Please check the URL or try navigating to a different page.</p>
         </div>
     );
 }
