e84fecc972940e44482ab35ea8fd5cc739e3214e
Gary Pang <Gary_Pang@comcast.com>
2024-03-09 12:54:05 -0500

    Add catch-all route for showing 404 page
    Add switch statement to render routes.
    Add route with wildcard path to catch URL paths that don't match existing routes.
    Comment out BodyBackgroundColor code that caused switch statement to render routes incorrectly.

--- frontend/src/src/App.js
+++ frontend/src/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom"; // removed Switch
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import { AuthProvider } from "./context/auth";
 import { UserProvider } from "./context/user";
@@ -10,9 +10,13 @@ import LandingPage from "../src/components/LandingPage";
 import FileUpload from "../src/components/FileUpload";
 import GeneratePage from "./components/GeneratePage";
 import ProfilePage from "../src/components/ProfilePage";
-import BodyBackgroundColor from "react-body-backgroundcolor";
+// TODO: Remove code related to BodyBackgroundColor before submitting PR.
+//  See Issue #161, "[Feature Request] 404 Page."
+
+// import BodyBackgroundColor from "react-body-backgroundcolor";
 import SignUp from "./components/SignUp/signUp";
 import Nav from "./components/nav";
+import PageNotFound from "./components/PageNotFound";
 
 function App(props) {
   return (
@@ -20,37 +24,43 @@ function App(props) {
       <UserProvider>
         <Router>
           <Nav />
-          <BodyBackgroundColor backgroundColor="#d9ecf9">
-            <Route path="/login" render={props => <LoginForm {...props} isAuthed={true} />} />
-          </BodyBackgroundColor>
+          <Switch>
+            {/* <BodyBackgroundColor backgroundColor="#d9ecf9"> */}
+              <Route path="/login" render={props => <LoginForm {...props} isAuthed={true} />} />
+            {/* </BodyBackgroundColor> */}
 
-          <BodyBackgroundColor backgroundColor="#d9ecf9">
-            <Route path="/signup" component={SignUp} />
-          </BodyBackgroundColor>
+            {/* <BodyBackgroundColor backgroundColor="#d9ecf9"> */}
+              <Route path="/signup" component={SignUp} />
+            {/* </BodyBackgroundColor> */}
 
-          <BodyBackgroundColor backgroundColor="gray">
-            <PrivateRoute exact path="/" component={LandingPage} />
-          </BodyBackgroundColor>
+            {/* <BodyBackgroundColor backgroundColor="gray"> */}
+              <PrivateRoute exact path="/" component={LandingPage} />
+            {/* </BodyBackgroundColor> */}
 
-          <BodyBackgroundColor backgroundColor="gray">
-            <PrivateRoute path="/action" component={ChooseAction} />
-          </BodyBackgroundColor>
+            {/* <BodyBackgroundColor backgroundColor="gray"> */}
+              <PrivateRoute path="/action" component={ChooseAction} />
+            {/* </BodyBackgroundColor> */}
 
-          <BodyBackgroundColor backgroundColor="#d9ecf9">
-            <PrivateRoute path="/search" component={SearchPage} />
-          </BodyBackgroundColor>
+            {/* <BodyBackgroundColor backgroundColor="#d9ecf9"> */}
+              <PrivateRoute path="/search" component={SearchPage} />
+            {/* </BodyBackgroundColor> */}
 
-          <BodyBackgroundColor backgroundColor="gray">
-            <PrivateRoute path="/upload" component={FileUpload} />
-          </BodyBackgroundColor>
+            {/* <BodyBackgroundColor backgroundColor="gray"> */}
+              <PrivateRoute path="/upload" component={FileUpload} />
+            {/* </BodyBackgroundColor> */}
 
-          <BodyBackgroundColor backgroundColor="#d9ecf9">
-            <PrivateRoute path="/generate" component={GeneratePage} />
-          </BodyBackgroundColor>
+            {/* <BodyBackgroundColor backgroundColor="#d9ecf9"> */}
+              <PrivateRoute path="/generate" component={GeneratePage} />
+            {/* </BodyBackgroundColor> */}
 
-          <BodyBackgroundColor backgroundColor="#d9ecf9">
-            <PrivateRoute path="/profile" component={ProfilePage} />
-          </BodyBackgroundColor>
+            {/* <BodyBackgroundColor backgroundColor="#d9ecf9"> */}
+              <PrivateRoute path="/profile" component={ProfilePage} />
+            {/* </BodyBackgroundColor> */}
+
+            {/* <BodyBackgroundColor backgroundColor="#d9ecf9"> */}
+              <PrivateRoute path="*" component={PageNotFound} />
+            {/* </BodyBackgroundColor> */}
+          </Switch>
         </Router>
       </UserProvider>
     </AuthProvider>
